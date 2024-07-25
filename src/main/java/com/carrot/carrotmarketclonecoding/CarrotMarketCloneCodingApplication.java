4d37a644458286e1a1c379dd59d71f04ebbd5f0a
honee8583 <honee8583@naver.com>
2024-07-25 18:21:44 +0900

    [CHORE]: 기본 프로젝트 생성 #2

---
+++ src/main/java/com/carrot/carrotmarketclonecoding/CarrotMarketCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.carrot.carrotmarketclonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotMarketCloneCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrotMarketCloneCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carrot/carrotmarketclonecoding/CarrotMarketCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrot.carrotmarketclonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotMarketCloneCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
