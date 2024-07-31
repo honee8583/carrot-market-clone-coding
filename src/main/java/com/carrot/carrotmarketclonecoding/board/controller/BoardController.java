91d7147421f2c52049eca710b261c62744e87fff
honee8583 <honee8583@naver.com>
2024-07-31 18:47:56 +0900

    [FEAT]: 게시글 저장 컨트롤러단 작성
    - 임의로 memberId는 1로 지정(회원기능 생성 전)

---
+++ src/main/java/com/carrot/carrotmarketclonecoding/board/controller/BoardController.java
@@ -0,0 +1,31 @@
+package com.carrot.carrotmarketclonecoding.board.controller;
+
+import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.BOARD_REGISTER_SUCCESS;
+
+import com.carrot.carrotmarketclonecoding.board.dto.BoardRequestDto.BoardRegisterRequestDto;
+import com.carrot.carrotmarketclonecoding.board.service.BoardService;
+import com.carrot.carrotmarketclonecoding.common.response.ResponseResult;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    @PostMapping("/board/register")
+    public ResponseEntity<?> register(@RequestBody @Valid BoardRegisterRequestDto registerRequestDto) {
+        // TODO memberId -> JWT.getMemberId()
+        Long memberId = 1L;
+
+        boardService.register(registerRequestDto, memberId);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ResponseResult.success(HttpStatus.CREATED, BOARD_REGISTER_SUCCESS.getMessage(), null));
+    }
+}
