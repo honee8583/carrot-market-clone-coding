3964cb51502bc8bc877cf13df5054a5ae1db86d8
honee8583 <honee8583@naver.com>
2024-07-31 18:43:14 +0900

    [FEAT]: 거래글 작성 입력 dto의 enum 필드 유효성검사용 클래스 및 어노테이션 생성
    - 일치하는 enum값이 존재하지 않거나 입력한 값이 없을 경우 에러발생 및 잘못된 입력 내용 전달

---
+++ src/main/java/com/carrot/carrotmarketclonecoding/common/exception/ValidEnum.java
@@ -0,0 +1,18 @@
+package com.carrot.carrotmarketclonecoding.common.exception;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = EnumValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidEnum {
+    String message() default "잘못된 입력입니다!";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<? extends java.lang.Enum<?>> enumClass();
+}
