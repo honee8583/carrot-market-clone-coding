ca91f2fa335925eeb89cfce71757f94c08a0283c
honee8583 <honee8583@naver.com>
2024-08-06 18:21:03 +0900

    [FEAT]: 10개 초과의 파일을 업로드 시도할 경우 반환할 에러 메시지및 예외 클래스 생성

--- src/main/java/com/carrot/carrotmarketclonecoding/common/response/FailedMessage.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/common/response/FailedMessage.java
@@ -11,7 +11,8 @@ public enum FailedMessage {
     INPUT_NOT_VALID("입력값이 잘못되었습니다!"),
     FILE_EXTENSION_NOT_VALID("png 혹은 jpeg/jpg 파일이 아닙니다!"),
     FILE_UPLOAD_FAILED("파일 업로드에 실패하였습니다!"),
-    FILE_NOT_EXISTS("파일이 존재하지 않습니다!");
+    FILE_NOT_EXISTS("파일이 존재하지 않습니다!"),
+    FILE_UPLOAD_LIMIT("업로드 가능한 파일은 10개까지입니다!");
 
     private final String message;
 }
