269266d100269fd82767ec35117af8c9151b3e4b
honee8583 <honee8583@naver.com>
2024-08-06 16:49:33 +0900

    [FEAT]: BoardPicture 엔티티 추가
    - 엔티티 및 repository 추가

---
+++ src/main/java/com/carrot/carrotmarketclonecoding/board/repository/BoardPictureRepository.java
@@ -0,0 +1,8 @@
+package com.carrot.carrotmarketclonecoding.board.repository;
+
+import com.carrot.carrotmarketclonecoding.board.domain.BoardPicture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardPictureRepository extends JpaRepository<BoardPicture, Long> {
+
+}
