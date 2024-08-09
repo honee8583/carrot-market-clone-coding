1764b24ed92ffa6a0c11420c66689d907c89715d
honee8583 <honee8583@naver.com>
2024-08-09 17:02:16 +0900

    [FEAT]: 게시글 조회 관련 성공/실패 메시지 추가

--- src/main/java/com/carrot/carrotmarketclonecoding/common/response/SuccessMessage.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/common/response/SuccessMessage.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SuccessMessage {
-    BOARD_REGISTER_SUCCESS("게시글 작성에 성공하였습니다!");
+    BOARD_REGISTER_SUCCESS("게시글 작성에 성공하였습니다!"),
+    BOARD_GET_DETAIL_SUCCESS("게시글 조회에 성공하였습니다!");
 
     private final String message;
 }
