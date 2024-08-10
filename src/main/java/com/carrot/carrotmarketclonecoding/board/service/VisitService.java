21e94df21f6092c6ae2507aaba38fc3db5fa68ae
honee8583 <93666531+honee8583@users.noreply.github.com>
2024-08-10 18:33:38 +0900

    [BUG]: 사용자 조회이력 공유 버그 해결 (#27)

--- src/main/java/com/carrot/carrotmarketclonecoding/board/service/VisitService.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/board/service/VisitService.java
@@ -3,7 +3,6 @@ package com.carrot.carrotmarketclonecoding.board.service;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,14 +10,16 @@ import org.springframework.stereotype.Service;
 public class VisitService {
     private final RedisTemplate<String, String> redisTemplate;
 
-    private static final String BOARD_VISIT_KEY = "board:visit:";
+    private static final String BOARD_VISIT_KEY_PREFIX = "board:visit:";
 
     public boolean increaseVisit(String boardId, String memberId) {
-        String visitKey = BOARD_VISIT_KEY + boardId;
-        SetOperations<String, String> setOps = redisTemplate.opsForSet();
-        boolean isFirstTimeView =  setOps.add(visitKey, memberId) > 0;
-        redisTemplate.expire(visitKey, 1, TimeUnit.DAYS);
+        String key = BOARD_VISIT_KEY_PREFIX + boardId + ":" + memberId;
+        Boolean memberViewed = redisTemplate.hasKey(key);
+        if (memberViewed != null && !memberViewed) {
+            redisTemplate.opsForValue().set(key, "1", 24, TimeUnit.HOURS);
+            return true;
+        }
 
-        return isFirstTimeView;
+        return false;
     }
 }
