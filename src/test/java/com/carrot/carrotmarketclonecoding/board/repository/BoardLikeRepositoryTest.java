31aee6f751580027540f14978bd33e077b1f3eb9
honee8583 <honee8583@naver.com>
2024-08-10 16:36:29 +0900

    [TEST]: 쿼리메소드 테스트 코드 추가

---
+++ src/test/java/com/carrot/carrotmarketclonecoding/board/repository/BoardLikeRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.carrot.carrotmarketclonecoding.board.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.carrot.carrotmarketclonecoding.board.domain.Board;
+import com.carrot.carrotmarketclonecoding.board.domain.BoardLike;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class BoardLikeRepositoryTest {
+
+    @Autowired
+    private BoardLikeRepository boardLikeRepository;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    @DisplayName("쿼리메소드 countByBoard() 테스트")
+    void countByBoard() {
+        // given
+        Board board = boardRepository.save(Board.builder().build());
+        boardLikeRepository.save(BoardLike.builder().board(board).build());
+        boardLikeRepository.save(BoardLike.builder().board(board).build());
+
+        // when
+        int result = boardLikeRepository.countByBoard(board);
+
+        // then
+        assertThat(result).isEqualTo(2);
+    }
+}
