d7b71040c239a19e4a963f42c887143baddbed86
honee8583 <honee8583@naver.com>
2024-08-18 17:32:18 +0900

    [FEAT]: 업데이트 서비스 메소드 추가

--- src/main/java/com/carrot/carrotmarketclonecoding/board/service/BoardService.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/board/service/BoardService.java
@@ -1,10 +1,13 @@
 package com.carrot.carrotmarketclonecoding.board.service;
 
 import com.carrot.carrotmarketclonecoding.board.dto.BoardRequestDto.BoardRegisterRequestDto;
+import com.carrot.carrotmarketclonecoding.board.dto.BoardRequestDto.BoardUpdateRequestDto;
 import com.carrot.carrotmarketclonecoding.board.dto.BoardResponseDto.BoardDetailResponseDto;
 
 public interface BoardService {
-    Long register(BoardRegisterRequestDto inputRequestDto, Long memberId, boolean tmp);
+    Long register(BoardRegisterRequestDto registerRequestDto, Long memberId, boolean tmp);
 
     BoardDetailResponseDto detail(Long boardId, String sessionId);
+
+    void update(BoardUpdateRequestDto updateRequestDto, Long boardId, Long memberId);
 }
