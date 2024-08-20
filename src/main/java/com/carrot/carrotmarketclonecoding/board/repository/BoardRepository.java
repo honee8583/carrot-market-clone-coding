eab026acecae0884a6d429b38d102434608df603
honee8583 <honee8583@naver.com>
2024-08-20 21:20:36 +0900

    [FEAT]: 게시글 Repository 확장

--- src/main/java/com/carrot/carrotmarketclonecoding/board/repository/BoardRepository.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/board/repository/BoardRepository.java
@@ -5,7 +5,7 @@ import com.carrot.carrotmarketclonecoding.member.domain.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
     void deleteAllByMemberAndTmpIsTrueAndIdIsNot(Member member, Long id);
     Optional<Board> findFirstByMemberAndTmpIsTrueOrderByCreateDateDesc(Member member);
 }
