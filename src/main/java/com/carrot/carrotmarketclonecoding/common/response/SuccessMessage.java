ca6f14eb4147dcd373a796836c6845e63161da2e
honee8583 <honee8583@naver.com>
2024-08-21 18:07:40 +0900

    [FEAT]: 관심게시글 등록 성공 메시지 추가

--- src/main/java/com/carrot/carrotmarketclonecoding/common/response/SuccessMessage.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/common/response/SuccessMessage.java
@@ -11,7 +11,8 @@ public enum SuccessMessage {
     BOARD_REGISTER_TEMPORARY_SUCCESS("게시글 임시저장에 성공하였습니다!"),
     BOARD_UPDATE_SUCCESS("게시글 수정에 성공하였습니다!"),
     BOARD_DELETE_SUCCESS("게시글 삭제에 성공하였습니다!"),
-    BOARD_GET_TMP_SUCCESS("이미시 게시글 조회에 성공하였습니다!");
+    BOARD_GET_TMP_SUCCESS("임시 게시글 조회에 성공하였습니다!"),
+    ADD_BOARD_LIKE_SUCCESS("관심게시글 등록에 성공하였습니다!");
 
     private final String message;
 }
