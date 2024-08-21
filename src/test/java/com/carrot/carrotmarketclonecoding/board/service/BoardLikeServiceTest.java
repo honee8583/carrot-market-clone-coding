fe683876fcb7c4be69826b48806aebdc7ffa2779
honee8583 <honee8583@naver.com>
2024-08-21 22:19:38 +0900

    [TEST]: 관심게시글 목록 조회 테스트 코드 추가

--- src/test/java/com/carrot/carrotmarketclonecoding/board/service/BoardLikeServiceTest.java
+++ src/test/java/com/carrot/carrotmarketclonecoding/board/service/BoardLikeServiceTest.java
@@ -2,19 +2,25 @@ package com.carrot.carrotmarketclonecoding.board.service;
 
 import static com.carrot.carrotmarketclonecoding.common.response.FailedMessage.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.carrot.carrotmarketclonecoding.board.domain.Board;
 import com.carrot.carrotmarketclonecoding.board.domain.BoardLike;
+import com.carrot.carrotmarketclonecoding.board.dto.BoardResponseDto.BoardSearchResponseDto;
 import com.carrot.carrotmarketclonecoding.board.repository.BoardLikeRepository;
 import com.carrot.carrotmarketclonecoding.board.repository.BoardRepository;
 import com.carrot.carrotmarketclonecoding.board.service.impl.BoardLikeServiceImpl;
 import com.carrot.carrotmarketclonecoding.common.exception.BoardNotFoundException;
 import com.carrot.carrotmarketclonecoding.common.exception.MemberNotFoundException;
+import com.carrot.carrotmarketclonecoding.common.response.PageResponseDto;
 import com.carrot.carrotmarketclonecoding.member.domain.Member;
 import com.carrot.carrotmarketclonecoding.member.repository.MemberRepository;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -24,6 +30,9 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
 class BoardLikeServiceTest {
@@ -50,8 +59,8 @@ class BoardLikeServiceTest {
             // given
             Long boardId = 1L;
             Long memberId = 1L;
-            Board mockBoard = Board.builder().id(boardId).build();
-            Member mockMember = Member.builder().id(memberId).build();
+            Board mockBoard = mock(Board.class);
+            Member mockMember = mock(Member.class);
 
             when(boardRepository.findById(anyLong())).thenReturn(Optional.of(mockBoard));
             when(memberRepository.findById(anyLong())).thenReturn(Optional.of(mockMember));
@@ -88,7 +97,7 @@ class BoardLikeServiceTest {
             // given
             Long boardId = 1L;
             Long memberId = 1L;
-            Board mockBoard = Board.builder().id(boardId).build();
+            Board mockBoard = mock(Board.class);
 
             when(boardRepository.findById(anyLong())).thenReturn(Optional.of(mockBoard));
             when(memberRepository.findById(anyLong())).thenReturn(Optional.empty());
@@ -96,6 +105,46 @@ class BoardLikeServiceTest {
             // when
             // then
             assertThatThrownBy(() -> boardLikeService.add(boardId, memberId))
+                    .isInstanceOf(MemberNotFoundException.class)
+                    .hasMessage(MEMBER_NOT_FOUND.getMessage());
+        }
+    }
+
+    @Nested
+    @DisplayName("사용자가 등록한 관심게시글 목록 조회")
+    class MemberLikedBoards {
+
+        @Test
+        @DisplayName("성공")
+        void memberLikedBoardsSuccess() {
+            // given
+            Long memberId = 1L;
+            Member mockMember = mock(Member.class);
+            Pageable pageable = PageRequest.of(0, 10);
+            List<BoardSearchResponseDto> boardSearchResponse = Arrays.asList(
+                    new BoardSearchResponseDto(),
+                    new BoardSearchResponseDto()
+            );
+
+            when(memberRepository.findById(anyLong())).thenReturn(Optional.of(mockMember));
+            when(boardRepository.searchMemberLikedBoards(any(), any())).thenReturn(new PageImpl<>(boardSearchResponse, pageable, boardSearchResponse.size()));
+
+            // when
+            PageResponseDto<BoardSearchResponseDto> result = boardLikeService.getMemberLikedBoards(memberId, pageable);
+
+            // then
+            assertThat(result.getContents().size()).isEqualTo(2);
+        }
+
+        @Test
+        @DisplayName("실패 - 사용자가 존재하지 않음")
+        void memberLikedBoardsFailMemberNotFound() {
+            // given
+            when(memberRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+            // when
+            // then
+            assertThatThrownBy(() -> boardLikeService.getMemberLikedBoards(1L, PageRequest.of(0, 10)))
                     .isInstanceOf(MemberNotFoundException.class)
                     .hasMessage(MEMBER_NOT_FOUND.getMessage());
         }
