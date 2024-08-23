986f16f59f8e102c2b4335eb1fb073e9e8c31549
honee8583 <honee8583@naver.com>
2024-08-23 20:42:27 +0900

    [FEAT]: 자주쓰는문구 추가 성공/실패 메시지 추가

--- src/main/java/com/carrot/carrotmarketclonecoding/common/response/SuccessMessage.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/common/response/SuccessMessage.java
@@ -14,7 +14,8 @@ public enum SuccessMessage {
     BOARD_GET_TMP_SUCCESS("임시 게시글 조회에 성공하였습니다!"),
     ADD_BOARD_LIKE_SUCCESS("관심게시글 등록에 성공하였습니다!"),
     GET_MEMBER_LIKED_BOARDS_SUCCESS("관심 게시글 목록 조회에 성공하였습니다!"),
-    SEARCH_BOARDS_SUCCESS("게시글 검색에 성공하였습니다!");
+    SEARCH_BOARDS_SUCCESS("게시글 검색에 성공하였습니다!"),
+    ADD_WORD_SUCCESS("자주쓰는문구 추가에 성공하였습니다!");
 
     private final String message;
 }
