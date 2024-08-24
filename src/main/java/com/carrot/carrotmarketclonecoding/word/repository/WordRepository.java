6b01325546241eb613889c36ad5d3014820d5c58
honee8583 <honee8583@naver.com>
2024-08-24 16:35:25 +0900

    [FEAT]: 사용자의 자주쓰는문구 개수를 조회하는 쿼리 메서드 추가

--- src/main/java/com/carrot/carrotmarketclonecoding/word/repository/WordRepository.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/word/repository/WordRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WordRepository extends JpaRepository<Word, Long> {
     List<Word> findAllByMember(Member member);
+    Integer countByMember(Member member);
 }
