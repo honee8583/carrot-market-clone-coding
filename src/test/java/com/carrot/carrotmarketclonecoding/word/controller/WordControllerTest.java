9288518885b281d00af1918e7c9c5bfa0d67aed5
honee8583 <honee8583@naver.com>
2024-08-24 14:48:49 +0900

    [TEST]: 자주쓰는문구 목록 조회 컨트롤러/서비스 테스트 케이스 추가

--- src/test/java/com/carrot/carrotmarketclonecoding/word/controller/WordControllerTest.java
+++ src/test/java/com/carrot/carrotmarketclonecoding/word/controller/WordControllerTest.java
@@ -3,20 +3,26 @@ package com.carrot.carrotmarketclonecoding.word.controller;
 import static com.carrot.carrotmarketclonecoding.common.response.FailedMessage.INPUT_NOT_VALID;
 import static com.carrot.carrotmarketclonecoding.common.response.FailedMessage.MEMBER_NOT_FOUND;
 import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.ADD_WORD_SUCCESS;
+import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.GET_MEMBER_WORDS;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.carrot.carrotmarketclonecoding.common.exception.MemberNotFoundException;
 import com.carrot.carrotmarketclonecoding.word.dto.WordRequestDto.WordRegisterRequestDto;
+import com.carrot.carrotmarketclonecoding.word.dto.WordResponseDto.WordListResponseDto;
 import com.carrot.carrotmarketclonecoding.word.dto.validation.WordRegisterValidationMessage.MESSAGE;
 import com.carrot.carrotmarketclonecoding.word.service.impl.WordServiceImpl;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -90,6 +96,48 @@ class WordControllerTest {
             mvc.perform(post("/word")
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(new ObjectMapper().writeValueAsString(new WordRegisterRequestDto("word"))))
+                    .andExpect(status().isUnauthorized())
+                    .andExpect(jsonPath("$.status", equalTo(401)))
+                    .andExpect(jsonPath("$.result", equalTo(false)))
+                    .andExpect(jsonPath("$.message", equalTo(MEMBER_NOT_FOUND.getMessage())))
+                    .andExpect(jsonPath("$.data", equalTo(null)));
+        }
+    }
+
+    @Nested
+    @DisplayName("자주쓰는문구 목록 조회 컨트롤러 테스트")
+    class WordList {
+
+        @Test
+        @DisplayName("성공")
+        void getWordsSuccess() throws Exception {
+            // given
+            List<WordListResponseDto> words = Arrays.asList(
+                    new WordListResponseDto(1L, "word1"),
+                    new WordListResponseDto(2L, "word2")
+            );
+
+            // when
+            when(wordService.list(anyLong())).thenReturn(words);
+
+            // then
+            mvc.perform(get("/word"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status", equalTo(200)))
+                    .andExpect(jsonPath("$.result", equalTo(true)))
+                    .andExpect(jsonPath("$.message", equalTo(GET_MEMBER_WORDS.getMessage())))
+                    .andExpect(jsonPath("$.data.size()", equalTo(2)));
+        }
+
+        @Test
+        @DisplayName("실패 - 사용자가 존재하지 않음")
+        void getWordsFailMemberNotFound() throws Exception {
+            // given
+            // when
+            doThrow(MemberNotFoundException.class).when(wordService).list(anyLong());
+
+            // then
+            mvc.perform(get("/word"))
                     .andExpect(status().isUnauthorized())
                     .andExpect(jsonPath("$.status", equalTo(401)))
                     .andExpect(jsonPath("$.result", equalTo(false)))
