3eff81bb2af77ebd52c45bc69df53c69ca40abef
honee8583 <honee8583@naver.com>
2024-08-24 14:47:43 +0900

    [FEAT]: 자주쓰는문구 목록 조회 서비스 메서드 추가

--- src/main/java/com/carrot/carrotmarketclonecoding/word/service/impl/WordServiceImpl.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/word/service/impl/WordServiceImpl.java
@@ -5,8 +5,10 @@ import com.carrot.carrotmarketclonecoding.member.domain.Member;
 import com.carrot.carrotmarketclonecoding.member.repository.MemberRepository;
 import com.carrot.carrotmarketclonecoding.word.domain.Word;
 import com.carrot.carrotmarketclonecoding.word.dto.WordRequestDto.WordRegisterRequestDto;
+import com.carrot.carrotmarketclonecoding.word.dto.WordResponseDto.WordListResponseDto;
 import com.carrot.carrotmarketclonecoding.word.repository.WordRepository;
 import com.carrot.carrotmarketclonecoding.word.service.WordService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,5 +25,13 @@ public class WordServiceImpl implements WordService {
         Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
         Word word = Word.createWord(registerRequestDto, member);
         wordRepository.save(word);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<WordListResponseDto> list(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+        List<Word> words = wordRepository.findAllByMember(member);
+        return words.stream().map(WordListResponseDto::createWordListResponseDto).toList();
     }
 }
