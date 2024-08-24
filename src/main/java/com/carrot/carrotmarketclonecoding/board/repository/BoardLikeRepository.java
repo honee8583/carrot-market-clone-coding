c3a6cc3c08e8342eb4ac165d17e4d822d5a9cab5
honee8583 <honee8583@naver.com>
2024-08-24 15:12:41 +0900

    [FEAT]: Member와 Board 엔티티로 BoardLike를 조회하는 쿼리메서드 추가

--- src/main/java/com/carrot/carrotmarketclonecoding/board/repository/BoardLikeRepository.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/board/repository/BoardLikeRepository.java
@@ -2,8 +2,11 @@ package com.carrot.carrotmarketclonecoding.board.repository;
 
 import com.carrot.carrotmarketclonecoding.board.domain.Board;
 import com.carrot.carrotmarketclonecoding.board.domain.BoardLike;
+import com.carrot.carrotmarketclonecoding.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardLikeRepository extends JpaRepository<BoardLike, Long> {
     int countByBoard(Board board);
+    Optional<BoardLike> findByBoardAndMember(Board board, Member member);
 }
