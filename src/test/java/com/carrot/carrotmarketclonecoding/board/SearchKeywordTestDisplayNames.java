188a21785f5816943afe4afe21261915ed1d62b9
honee8583 <honee8583@naver.com>
2024-08-29 21:08:26 +0900

    [TEST]: 사용자의 최근 검색어 목록 조회 테스트 케이스 추가

--- src/test/java/com/carrot/carrotmarketclonecoding/board/SearchKeywordTestDisplayNames.java
+++ src/test/java/com/carrot/carrotmarketclonecoding/board/SearchKeywordTestDisplayNames.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SearchKeywordTestDisplayNames {
     SEARCH_KEYWORD_TOP_RANK_CONTROLLER_TEST(MESSAGE.SEARCH_KEYWORD_TOP_RANK_CONTROLLER_TEST),
+    RECENT_SEARCH_KEYWORD_CONTROLLER_TEST(MESSAGE.RECENT_SEARCH_KEYWORD_CONTROLLER_TEST),
     SUCCESS(MESSAGE.SUCCESS);
 
     private final String message;
 
     public static class MESSAGE {
         public static final String SEARCH_KEYWORD_TOP_RANK_CONTROLLER_TEST = "인기검색어 목록 조회 컨트롤러 테스트";
+        public static final String RECENT_SEARCH_KEYWORD_CONTROLLER_TEST = "사용자의 최근 검색어 목록 조회 컨트롤러 테스트";
         public static final String SUCCESS = "성공";
     }
 }
--- src/test/java/com/carrot/carrotmarketclonecoding/board/controller/SearchKeywordControllerTest.java
+++ src/test/java/com/carrot/carrotmarketclonecoding/board/controller/SearchKeywordControllerTest.java
@@ -3,13 +3,16 @@ package com.carrot.carrotmarketclonecoding.board.controller;
 import static com.carrot.carrotmarketclonecoding.board.SearchKeywordTestDisplayNames.MESSAGE.*;
 import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.*;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.carrot.carrotmarketclonecoding.board.service.SearchKeywordService;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -49,6 +52,31 @@ class SearchKeywordControllerTest {
                     .andExpect(jsonPath("$.status", equalTo(200)))
                     .andExpect(jsonPath("$.result", equalTo(true)))
                     .andExpect(jsonPath("$.message", equalTo(GET_TOP_RANK_SEARCH_KEYWORDS_SUCCESS.getMessage())))
+                    .andExpect(jsonPath("$.data").isArray())
+                    .andExpect(jsonPath("$.data[0]", equalTo("keyword1")))
+                    .andExpect(jsonPath("$.data[1]", equalTo("keyword2")));
+        }
+    }
+
+    @Nested
+    @DisplayName(RECENT_SEARCH_KEYWORD_CONTROLLER_TEST)
+    class RecentSearchKeywords {
+
+        @Test
+        @DisplayName(SUCCESS)
+        void recentSearchKeywordSuccess() throws Exception {
+            // given
+            List<String> keywords = Arrays.asList("keyword1", "keyword2");
+
+            // when
+            when(searchKeywordService.getRecentSearches(anyLong())).thenReturn(keywords);
+
+            // then
+            mvc.perform(get("/search/recent"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status", equalTo(200)))
+                    .andExpect(jsonPath("$.result", equalTo(true)))
+                    .andExpect(jsonPath("$.message", equalTo(GET_RECENT_SEARCH_KEYWORDS_SUCCESS.getMessage())))
                     .andExpect(jsonPath("$.data").isArray())
                     .andExpect(jsonPath("$.data[0]", equalTo("keyword1")))
                     .andExpect(jsonPath("$.data[1]", equalTo("keyword2")));
