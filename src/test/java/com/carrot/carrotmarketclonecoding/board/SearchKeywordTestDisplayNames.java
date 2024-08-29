0aebb331527c40bf714016fdb099508dd7edf43d
honee8583 <honee8583@naver.com>
2024-08-29 20:45:54 +0900

    [TEST]: 인기검색어 조회 컨트롤러 테스트 케이스 추가

---
+++ src/test/java/com/carrot/carrotmarketclonecoding/board/SearchKeywordTestDisplayNames.java
@@ -0,0 +1,18 @@
+package com.carrot.carrotmarketclonecoding.board;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SearchKeywordTestDisplayNames {
+    SEARCH_KEYWORD_TOP_RANK_CONTROLLER_TEST(MESSAGE.SEARCH_KEYWORD_TOP_RANK_CONTROLLER_TEST),
+    SUCCESS(MESSAGE.SUCCESS);
+
+    private final String message;
+
+    public static class MESSAGE {
+        public static final String SEARCH_KEYWORD_TOP_RANK_CONTROLLER_TEST = "인기검색어 목록 조회 컨트롤러 테스트";
+        public static final String SUCCESS = "성공";
+    }
+}
