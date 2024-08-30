a676982fd216a91760c9f1549097e693f52371e3
honee8583 <honee8583@naver.com>
2024-08-30 17:19:12 +0900

    [TEST]: 사용자의 특정 최근 검색어 삭제 컨트롤러 테스트 케이스 추가

--- src/test/java/com/carrot/carrotmarketclonecoding/board/controller/SearchKeywordControllerTest.java
+++ src/test/java/com/carrot/carrotmarketclonecoding/board/controller/SearchKeywordControllerTest.java
@@ -1,15 +1,22 @@
 package com.carrot.carrotmarketclonecoding.board.controller;
 
 import static com.carrot.carrotmarketclonecoding.board.SearchKeywordTestDisplayNames.MESSAGE.*;
+import static com.carrot.carrotmarketclonecoding.common.response.FailedMessage.MEMBER_NOT_FOUND;
 import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.*;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.carrot.carrotmarketclonecoding.board.service.SearchKeywordService;
+import com.carrot.carrotmarketclonecoding.board.service.impl.SearchKeywordServiceImpl;
+import com.carrot.carrotmarketclonecoding.common.exception.MemberNotFoundException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -20,6 +27,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(SearchKeywordController.class)
@@ -29,7 +37,7 @@ class SearchKeywordControllerTest {
     private MockMvc mvc;
 
     @MockBean
-    private SearchKeywordService searchKeywordService;
+    private SearchKeywordServiceImpl searchKeywordService;
 
     @Nested
     @DisplayName(SEARCH_KEYWORD_TOP_RANK_CONTROLLER_TEST)
@@ -44,7 +52,7 @@ class SearchKeywordControllerTest {
             keywords.add("keyword2");
 
             // when
-            when(searchKeywordService.getTopSearchRank()).thenReturn(keywords);
+            when(searchKeywordService.getSearchKeywordRank()).thenReturn(keywords);
 
             // then
             mvc.perform(get("/search/rank"))
@@ -69,7 +77,7 @@ class SearchKeywordControllerTest {
             List<String> keywords = Arrays.asList("keyword1", "keyword2");
 
             // when
-            when(searchKeywordService.getRecentSearches(anyLong())).thenReturn(keywords);
+            when(searchKeywordService.getRecentSearchKeywords(anyLong())).thenReturn(keywords);
 
             // then
             mvc.perform(get("/search/recent"))
@@ -80,6 +88,65 @@ class SearchKeywordControllerTest {
                     .andExpect(jsonPath("$.data").isArray())
                     .andExpect(jsonPath("$.data[0]", equalTo("keyword1")))
                     .andExpect(jsonPath("$.data[1]", equalTo("keyword2")));
+        }
+
+        @Test
+        @DisplayName(FAIL_MEMBER_NOT_FOUND)
+        void recentSearchKeywordFailMemberNotFound() throws Exception {
+            // given
+
+            // when
+            doThrow(MemberNotFoundException.class).when(searchKeywordService).getRecentSearchKeywords(anyLong());
+
+            // then
+            mvc.perform(get("/search/recent"))
+                    .andExpect(status().isUnauthorized())
+                    .andExpect(jsonPath("$.status", equalTo(401)))
+                    .andExpect(jsonPath("$.result", equalTo(false)))
+                    .andExpect(jsonPath("$.message", equalTo(MEMBER_NOT_FOUND.getMessage())))
+                    .andExpect(jsonPath("$.data", equalTo(null)));
+        }
+    }
+
+    @Nested
+    @DisplayName(REMOVE_RECENT_SEARCH_KEYWORD_CONTROLLER_TEST)
+    class RemoveRecentSearchKeyword {
+
+        @Test
+        @DisplayName(SUCCESS)
+        void removeRecentSearchKeywordSuccess() throws Exception {
+            // given
+            // when
+            doNothing().when(searchKeywordService).removeRecentSearchKeyword(anyLong(), anyString());
+
+            // then
+            mvc.perform(delete("/search/recent")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .param("keyword", "keyword"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status", equalTo(200)))
+                    .andExpect(jsonPath("$.result", equalTo(true)))
+                    .andExpect(jsonPath("$.message", equalTo(REMOVE_RECENT_SEARCH_KEYWORD_SUCCESS.getMessage())))
+                    .andExpect(jsonPath("$.data", equalTo(null)));
+        }
+
+        @Test
+        @DisplayName(FAIL_MEMBER_NOT_FOUND)
+        void removeRecentSearchKeywordFailMemberNotFound() throws Exception {
+            // given
+
+            // when
+            doThrow(MemberNotFoundException.class).when(searchKeywordService).removeRecentSearchKeyword(anyLong(), any());
+
+            // then
+            mvc.perform(delete("/search/recent")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .param("keyword", "keyword"))
+                    .andExpect(status().isUnauthorized())
+                    .andExpect(jsonPath("$.status", equalTo(401)))
+                    .andExpect(jsonPath("$.result", equalTo(false)))
+                    .andExpect(jsonPath("$.message", equalTo(MEMBER_NOT_FOUND.getMessage())))
+                    .andExpect(jsonPath("$.data", equalTo(null)));
         }
     }
 }
