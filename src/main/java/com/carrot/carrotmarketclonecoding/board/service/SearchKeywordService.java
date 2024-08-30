6bc0293df4401a9f9a835f049610c68f5f006f04
honee8583 <93666531+honee8583@users.noreply.github.com>
2024-08-30 17:23:42 +0900

    사용자의 특정 최근 검색어 삭제 기능 구현 (#69)
    * [FEAT]: 사용자의 최근 검색어 삭제 성공 메시지 추가
    
    * [FEAT]: 사용자의 최근 검색어 삭제 로직 구현 및 사용자 존재 체크 로직 추가
    - 사용자의 특정 최근 검색어 삭제 로직 구현
    - 검색어 추가/삭제를 요청한 사용자가 존재하는지 확인 로직 구현
    - 검색어 서비스 인터페이스 생성
    - addMemberSearchKeywords() -> addRecentSearchKeywords() 메서드명 변경
    
    * [MODIFY]: 사용자의 최근 검색어 추가 메서드명 변경된 부분 반영
    * [FEAT]: 사용자의 특정 최근 검색어 삭제 컨트롤러 메서드 추가
    
    * [TEST]: 사용자의 특정 최근 검색어 삭제 서비스 테스트 케이스 추가
    - 검색어 서비스 테스트 메서드명 변경
    - testAddSearchRank() -> addSearchKeywordsRank()
    - testGetTopSearchRank() -> getTopSearchKeywordsRank()
    - testAddMemberSearchKeywords() -> addRecentSearchKeywords()
    - testAddMemberSearchKeywordsRemoveOldKeyword() -> addRecentSearchKeywordsRemoveOldKeyword()
    - testGetRecentSearches() -> getRecentSearchKeywords()
    
    * [TEST]: 사용자의 최근검색어 추가 메스드명 변경부분 반영
    * [TEST]: 사용자의 특정 최근 검색어 삭제 컨트롤러 테스트 케이스 추가
    
    * [TEST]: 검색어 관련 테스트 DisplayName 추가
    - 사용자의 특정 최근 검색어 삭제 컨트롤러 테스트
    - 사용자가 존재하지 않을 경우에 대한 테스트

--- src/main/java/com/carrot/carrotmarketclonecoding/board/service/SearchKeywordService.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/board/service/SearchKeywordService.java
@@ -2,46 +2,15 @@ package com.carrot.carrotmarketclonecoding.board.service;
 
 import java.util.List;
 import java.util.Set;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class SearchKeywordService {
-    private final RedisTemplate<String, String> redisTemplate;
+public interface SearchKeywordService {
+    void addSearchRank(String keyword);
 
-    private static final String SEARCH_RANK_KEY = "search:rank";
-    private static final String SEARCH_RECENT_KEY = "search:recent:";
-    private static final int MAX_RECENT_SEARCHES = 20;
+    Set<String> getSearchKeywordRank();
 
-    public void addSearchRank(String keyword) {
-        redisTemplate.opsForZSet().incrementScore(SEARCH_RANK_KEY, keyword, 1);
-    }
+    void addRecentSearchKeywords(Long memberId, String keyword);
 
-    public Set<String> getTopSearchRank() {
-        return redisTemplate.opsForZSet().reverseRange(SEARCH_RANK_KEY, 0, 9);
-    }
+    List<String> getRecentSearchKeywords(Long memberId);
 
-    public void addMemberSearchKeywords(Long memberId, String keyword) {
-        String key = SEARCH_RECENT_KEY + memberId;
-
-        ListOperations<String, String> listOperations = redisTemplate.opsForList();
-        listOperations.remove(key, 1, keyword);
-        listOperations.rightPush(key, keyword);
-
-        if (listOperations.size(key) > MAX_RECENT_SEARCHES) {
-            listOperations.leftPop(key);
-        }
-    }
-
-    public List<String> getRecentSearches(Long memberId) {
-        String key = SEARCH_RECENT_KEY + memberId;
-        return redisTemplate.opsForList().range(key, 0, -1);
-    }
+    void removeRecentSearchKeyword(Long memberId, String keyword);
 }
