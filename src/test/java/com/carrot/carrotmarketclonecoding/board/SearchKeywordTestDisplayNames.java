5e3e308c2ed955efcbadac2c4d889e82cfba4ee3
honee8583 <honee8583@naver.com>
2024-08-30 17:20:16 +0900

    [TEST]: 검색어 관련 테스트 DisplayName 추가
    - 사용자의 특정 최근 검색어 삭제 컨트롤러 테스트
    - 사용자가 존재하지 않을 경우에 대한 테스트

--- src/test/java/com/carrot/carrotmarketclonecoding/board/SearchKeywordTestDisplayNames.java
+++ src/test/java/com/carrot/carrotmarketclonecoding/board/SearchKeywordTestDisplayNames.java
@@ -8,13 +8,17 @@ import lombok.Getter;
 public enum SearchKeywordTestDisplayNames {
     SEARCH_KEYWORD_TOP_RANK_CONTROLLER_TEST(MESSAGE.SEARCH_KEYWORD_TOP_RANK_CONTROLLER_TEST),
     RECENT_SEARCH_KEYWORD_CONTROLLER_TEST(MESSAGE.RECENT_SEARCH_KEYWORD_CONTROLLER_TEST),
-    SUCCESS(MESSAGE.SUCCESS);
+    REMOVE_RECENT_SEARCH_KEYWORD_CONTROLLER_TEST(MESSAGE.REMOVE_RECENT_SEARCH_KEYWORD_CONTROLLER_TEST),
+    SUCCESS(MESSAGE.SUCCESS),
+    FAIL_MEMBER_NOT_FOUND(MESSAGE.FAIL_MEMBER_NOT_FOUND);
 
     private final String message;
 
     public static class MESSAGE {
         public static final String SEARCH_KEYWORD_TOP_RANK_CONTROLLER_TEST = "인기검색어 목록 조회 컨트롤러 테스트";
         public static final String RECENT_SEARCH_KEYWORD_CONTROLLER_TEST = "사용자의 최근 검색어 목록 조회 컨트롤러 테스트";
+        public static final String REMOVE_RECENT_SEARCH_KEYWORD_CONTROLLER_TEST = "사용자의 최근 검색어 삭제 컨트롤러 테스트";
         public static final String SUCCESS = "성공";
+        public static final String FAIL_MEMBER_NOT_FOUND = "사용자가 존재하지 않음";
     }
 }
