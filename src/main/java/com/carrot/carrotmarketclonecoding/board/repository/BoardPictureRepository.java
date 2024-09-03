14acc0e8be7ee7d3d6e31148dfab92f44539fa4a
honee8583 <honee8583@naver.com>
2024-09-03 16:32:57 +0900

    [FEAT]: 게시글 아이디를 사용해 관련된 BoardLike, BoardPicture 데이터를 모두 삭제하는 쿼리메서드 생성

--- src/main/java/com/carrot/carrotmarketclonecoding/board/repository/BoardPictureRepository.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/board/repository/BoardPictureRepository.java
@@ -4,7 +4,13 @@ import com.carrot.carrotmarketclonecoding.board.domain.Board;
 import com.carrot.carrotmarketclonecoding.board.domain.BoardPicture;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardPictureRepository extends JpaRepository<BoardPicture, Long> {
     List<BoardPicture> findByBoard(Board board);
+
+    @Modifying
+    @Query("DELETE FROM BoardPicture bp WHERE bp.board.id = :boardId")
+    void deleteAllByBoardId(Long boardId);
 }
