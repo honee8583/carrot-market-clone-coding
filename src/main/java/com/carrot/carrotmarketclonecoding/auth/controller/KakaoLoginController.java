bed62b8ef1b6bdf5cb0836a7a42979946038cfb8
honee8583 <honee8583@naver.com>
2024-09-13 17:29:18 +0900

    [MODIFY]: Slf4j import 문 제거

--- src/main/java/com/carrot/carrotmarketclonecoding/auth/controller/KakaoLoginController.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/auth/controller/KakaoLoginController.java
@@ -7,7 +7,6 @@ import com.carrot.carrotmarketclonecoding.auth.dto.TokenDto;
 import com.carrot.carrotmarketclonecoding.auth.service.LoginService;
 import com.carrot.carrotmarketclonecoding.common.response.ResponseResult;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class KakaoLoginController {
