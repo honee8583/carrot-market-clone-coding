57a69e68b4cfee803639fe8db9d7335bdf130735
honee8583 <honee8583@naver.com>
2024-09-14 17:06:28 +0900

    [FEAT]: 컨트롤러에 회원탈퇴 API 추가

--- src/main/java/com/carrot/carrotmarketclonecoding/auth/controller/KakaoLoginController.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/auth/controller/KakaoLoginController.java
@@ -3,13 +3,16 @@ package com.carrot.carrotmarketclonecoding.auth.controller;
 import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.LOGIN_SUCCESS;
 import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.RECREATE_TOKENS_SUCCESS;
 
+import com.carrot.carrotmarketclonecoding.auth.dto.LoginUser;
 import com.carrot.carrotmarketclonecoding.auth.dto.TokenDto;
 import com.carrot.carrotmarketclonecoding.auth.service.LoginService;
 import com.carrot.carrotmarketclonecoding.common.response.ResponseResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class KakaoLoginController {
-
     private final LoginService loginService;
 
     @GetMapping("/callback")
@@ -34,5 +36,13 @@ public class KakaoLoginController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ResponseResult.success(HttpStatus.OK, RECREATE_TOKENS_SUCCESS.getMessage(), tokens));
+    }
+
+    @PostMapping("/withdraw")
+    public ResponseEntity<?> withdraw(@AuthenticationPrincipal LoginUser loginUser) {
+        loginService.withdraw(Long.parseLong(loginUser.getUsername()));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseResult.success(HttpStatus.OK, "회원탈퇴에 성공하였습니다!", null));
     }
 }
