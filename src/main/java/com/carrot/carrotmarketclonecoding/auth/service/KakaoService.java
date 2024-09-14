c0fd89cc771dd927622824bb56bdbac0e96ea1b2
honee8583 <honee8583@naver.com>
2024-09-14 17:09:29 +0900

    [FEAT]: 카카오 로그인 서비스 레이어 로직 변경
    - 카카오 로그아웃, 연결해제를 위한 어드민 키, 링크, 파라미터 값 추가
    - ContentType 세팅 메서드 변경(set() -> setContentType())
    - Authorization 세팅 메서드 변경(set() -> setBearerAuth())
    - 카카오 로그아웃, 연결해제는 URL만 다르므로 동일한 메서드 사용

--- src/main/java/com/carrot/carrotmarketclonecoding/auth/service/KakaoService.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/auth/service/KakaoService.java
@@ -1,22 +1,20 @@
 package com.carrot.carrotmarketclonecoding.auth.service;
 
-import com.carrot.carrotmarketclonecoding.auth.dto.LoginRespDto;
-import com.carrot.carrotmarketclonecoding.common.exception.KakaoTokenNotExistsException;
-import com.carrot.carrotmarketclonecoding.common.exception.KakaoUserInfoNotExistsException;
 import com.carrot.carrotmarketclonecoding.auth.dto.KakaoTokenResponseDto;
 import com.carrot.carrotmarketclonecoding.auth.dto.KakaoUserInfoResponseDto;
-import com.carrot.carrotmarketclonecoding.member.domain.Member;
-import com.carrot.carrotmarketclonecoding.member.domain.enums.Role;
-import com.carrot.carrotmarketclonecoding.member.repository.MemberRepository;
-import java.util.Optional;
+import com.carrot.carrotmarketclonecoding.common.exception.KakaoTokenNotExistsException;
+import com.carrot.carrotmarketclonecoding.common.exception.KakaoUserInfoNotExistsException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -28,18 +26,24 @@ public class KakaoService {
     @Value("${kakao.client_id}")
     private String clientId;
 
+    @Value("${kakao.admin-key}")
+    private String adminKey;
+
     private static final String KAUTH_TOKEN_URL_HOST = "https://kauth.kakao.com/oauth/token";
     private static final String KAUTH_USER_URL_HOST = "https://kapi.kakao.com/v2/user/me";
-    private static final String KAKAO_REQUEST_HEADER = "Bearer ";
-    private static final String CONTENT_TYPE = "application/x-www-form-urlencoded";
-    private static final String PROPERTY_NICKNAME = "nickname";
-    private static final String PROPERTY_PROFILE_IMAGE = "profile_image";
+    private static final String KAUTH_UNLINK_URL_HOST = "https://kapi.kakao.com/v1/user/unlink";
+    private static final String KAUTH_LOGOUT_URL_HOST = "https://kapi.kakao.com/v1/user/logout";
+
     private static final String PARAM_GRANT_TYPE = "grant_type";
     private static final String PARAM_CLIENT_ID = "client_id";
     private static final String PARAM_CODE = "code";
     private static final String GRANT_TYPE_VALUE = "authorization_code";
+    private static final String PARAM_TARGET_ID_TYPE = "target_id_type";
+    private static final String PARAM_TARGET_ID = "target_id";
+    private static final String VALUE_USER_ID = "user_id";
 
-    private final MemberRepository memberRepository;
+    private static final String UNLINK_AND_LOGOUT_HEADER_PREFIX = "KakaoAK ";
+
     private final RestTemplate restTemplate;
 
     public String getAccessToken(String code) {
@@ -50,7 +54,7 @@ public class KakaoService {
                 .toUriString();
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
         ResponseEntity<KakaoTokenResponseDto> responseEntity = restTemplate.exchange(
@@ -65,18 +69,13 @@ public class KakaoService {
             throw new KakaoTokenNotExistsException();
         }
 
-        log.debug(" [Kakao Service] Access Token ------> {}", kakaoTokenResponseDto.getAccessToken());
-        log.debug(" [Kakao Service] Refresh Token ------> {}", kakaoTokenResponseDto.getRefreshToken());
-        log.debug(" [Kakao Service] Id Token ------> {}", kakaoTokenResponseDto.getIdToken());
-        log.debug(" [Kakao Service] Scope ------> {}", kakaoTokenResponseDto.getScope());
-
         return kakaoTokenResponseDto.getAccessToken();
     }
 
     public KakaoUserInfoResponseDto getUserInfo(String accessToken) {
         HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.AUTHORIZATION, KAKAO_REQUEST_HEADER + accessToken);
-        headers.set(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE);
+        headers.setBearerAuth(accessToken);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
         ResponseEntity<KakaoUserInfoResponseDto> responseEntity = restTemplate.exchange(
@@ -92,33 +91,34 @@ public class KakaoService {
         }
 
         log.debug("[ Kakao Service ] Auth ID ---> {} ", userInfo.getId());
-        log.debug("[ Kakao Service ] NickName ---> {} ", userInfo.getProperties().get(PROPERTY_NICKNAME));
-        log.debug("[ Kakao Service ] ProfileImageUrl ---> {} ", userInfo.getProperties().get(PROPERTY_PROFILE_IMAGE));
 
         return userInfo;
     }
 
-    public LoginRespDto join(KakaoUserInfoResponseDto userInfo) {
-        Optional<Member> optionalMember = memberRepository.findByAuthId(userInfo.getId());
-        Member member = null;
+    public void logout(Long authId) {
+        kakaoRequest(authId, KAUTH_LOGOUT_URL_HOST);
+    }
 
-        if (optionalMember.isPresent()) {
-            member = optionalMember.get();
-        } else {
-            member = memberRepository.save(Member.builder()
-                    .authId(userInfo.getId())
-                    .nickname(userInfo.getProperties().get(PROPERTY_NICKNAME))
-                    .profileUrl(userInfo.getProperties().get(PROPERTY_PROFILE_IMAGE))
-                    .town(null)
-                    .withdraw(false)
-                    .isTownAuthenticated(false)
-                    .role(Role.USER)
-                    .build());
-        }
+    public void unlink(Long authId) {
+        kakaoRequest(authId, KAUTH_UNLINK_URL_HOST);
+    }
 
-        return LoginRespDto.builder()
-                .authId(member.getAuthId())
-                .role(member.getRole())
-                .build();
+    private void kakaoRequest(Long authId, String url) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.set(HttpHeaders.AUTHORIZATION, UNLINK_AND_LOGOUT_HEADER_PREFIX + adminKey);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add(PARAM_TARGET_ID_TYPE, VALUE_USER_ID);
+        params.add(PARAM_TARGET_ID, String.valueOf(authId));
+
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params, headers);
+        ResponseEntity<String> result = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                entity,
+                String.class);
+
+        log.debug("[ Kakao Service ] unlink, logout result ---> {} ", result.getBody());
     }
 }
