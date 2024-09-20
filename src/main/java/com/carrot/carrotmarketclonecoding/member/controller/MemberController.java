888fdf1de09a0a00e9fb476a946a168386b7a6b6
honee8583 <honee8583@naver.com>
2024-09-21 01:11:30 +0900

    [FEAT]: 프로필 정보 조회 컨트롤러 메서드 추가

--- src/main/java/com/carrot/carrotmarketclonecoding/member/controller/MemberController.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/member/controller/MemberController.java
@@ -1,15 +1,18 @@
 package com.carrot.carrotmarketclonecoding.member.controller;
 
+import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.PROFILE_DETAIL_SUCCESS;
 import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.PROFILE_UPDATE_SUCCESS;
 
 import com.carrot.carrotmarketclonecoding.auth.dto.LoginUser;
 import com.carrot.carrotmarketclonecoding.common.response.ResponseResult;
+import com.carrot.carrotmarketclonecoding.member.dto.ProfileDto.ProfileDetailResponseDto;
 import com.carrot.carrotmarketclonecoding.member.dto.ProfileDto.ProfileUpdateRequestDto;
 import com.carrot.carrotmarketclonecoding.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,4 +33,12 @@ public class MemberController {
                 .body(ResponseResult.success(HttpStatus.OK, PROFILE_UPDATE_SUCCESS.getMessage(), null));
     }
 
+    @GetMapping
+    public ResponseEntity<?> detail(@AuthenticationPrincipal LoginUser loginUser) {
+        Long authId = Long.parseLong(loginUser.getUsername());
+        ProfileDetailResponseDto profileDetail = memberService.detail(authId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseResult.success(HttpStatus.OK, PROFILE_DETAIL_SUCCESS.getMessage(), profileDetail));
+    }
 }
