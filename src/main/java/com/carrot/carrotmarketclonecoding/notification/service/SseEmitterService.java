a5de9eb04e8d8be5d5cc70be06fc238a9b730be8
honee8583 <honee8583@naver.com>
2024-09-25 16:26:12 +0900

    [FEAT]: SSE 서비스 클래스 생성
    - SSE 연결, SSE를 사용한 알림 전송 기능 구현

---
+++ src/main/java/com/carrot/carrotmarketclonecoding/notification/service/SseEmitterService.java
@@ -0,0 +1,60 @@
+package com.carrot.carrotmarketclonecoding.notification.service;
+
+import com.carrot.carrotmarketclonecoding.notification.domain.enums.NotificationType;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Slf4j
+@Component
+public class SseEmitterService {
+
+    @Value("${sse.timeout}")
+    private Long timeout;
+
+    private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    public SseEmitter add(Long authId) {
+        SseEmitter emitter = new SseEmitter(timeout);
+        this.emitters.put(authId, emitter);
+
+        log.debug("Sending Dummy Data...");
+        send(authId, NotificationType.NOTICE, "connected!");
+
+        log.debug("new member emitter added : {} : {}, size: {}", authId, emitter, emitters.size());
+
+        emitter.onCompletion(() -> {
+            log.debug("SseEmitter onCompletion callback");
+            this.emitters.remove(authId);
+        });
+
+        emitter.onTimeout(() -> {
+            log.debug("SseEmitter onTimeout callback");
+            emitter.complete();
+        });
+
+        emitter.onError(throwable -> {
+            log.debug("error");
+            emitter.complete();
+        });
+
+        return emitter;
+    }
+
+    public void send(Long id, NotificationType type, String content) {
+        SseEmitter emitter = emitters.get(id);
+        try {
+            log.debug("알림을 전송합니다!");
+            emitter.send(SseEmitter.event()
+                    .name(type.name())
+                    .data(content));
+        } catch (IOException e) {
+            log.error("알림 전송에 실패하였습니다!");
+            throw new RuntimeException(e);
+        }
+    }
+}
