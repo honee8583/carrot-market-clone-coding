c9711edc60583778da4a4863fafde513d7701a4f
honee8583 <honee8583@naver.com>
2024-09-25 16:33:26 +0900

    [FEAT]: 알림 응답 Dto 클래스 생성
    - id, 내용, 알림타입, 생성일, 수정일 포함

---
+++ src/main/java/com/carrot/carrotmarketclonecoding/notification/dto/NotificationResponseDto.java
@@ -0,0 +1,30 @@
+package com.carrot.carrotmarketclonecoding.notification.dto;
+
+import com.carrot.carrotmarketclonecoding.notification.domain.Notification;
+import com.carrot.carrotmarketclonecoding.notification.domain.enums.NotificationType;
+import java.time.LocalDateTime;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationResponseDto {
+    private Long id;
+    private String content;
+    private NotificationType type;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+    public static NotificationResponseDto createNotificationResponseDto(Notification notification) {
+        return NotificationResponseDto.builder()
+                .id(notification.getId())
+                .content(notification.getContent())
+                .type(notification.getType())
+                .createDate(notification.getCreateDate())
+                .updateDate(notification.getUpdateDate())
+                .build();
+    }
+}
