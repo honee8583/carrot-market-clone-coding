39db147681b45f55cd1481f9afe8e6cfd67afcb9
honee8583 <honee8583@naver.com>
2024-09-25 16:21:15 +0900

    [FEAT]: Notification 엔티티 클래스 생성
    - Member와 Notification은 다대일 연관관계
    - 알림내용, 읽음여부, 알림타입 지정

---
+++ src/main/java/com/carrot/carrotmarketclonecoding/notification/domain/Notification.java
@@ -0,0 +1,38 @@
+package com.carrot.carrotmarketclonecoding.notification.domain;
+
+import com.carrot.carrotmarketclonecoding.common.BaseEntity;
+import com.carrot.carrotmarketclonecoding.member.domain.Member;
+import com.carrot.carrotmarketclonecoding.notification.domain.enums.NotificationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "notifications")
+public class Notification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    private String content;
+
+    private boolean isRead;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType type;
+}
