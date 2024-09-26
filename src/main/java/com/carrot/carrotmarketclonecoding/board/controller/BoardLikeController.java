73b55cccfc4476cf78c0a479dd47b5b2b9e6787a
honee8583 <93666531+honee8583@users.noreply.github.com>
2024-09-26 17:12:05 +0900

    SSE 연결 문제 해결을 위한 로그 추가 (#101)
    * [CHORE]: 배포서버 로그 레벨 설정
    - SSE 문제 해결을 위해 로그 레벨 설정
    
    * [MODIFY]: 좋아요 등록 로그 추가
    - SSE 연결 문제를 해결하기 위한 로그 추가

--- src/main/java/com/carrot/carrotmarketclonecoding/board/controller/BoardLikeController.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/board/controller/BoardLikeController.java
@@ -9,6 +9,7 @@ import com.carrot.carrotmarketclonecoding.board.service.BoardLikeService;
 import com.carrot.carrotmarketclonecoding.common.response.PageResponseDto;
 import com.carrot.carrotmarketclonecoding.common.response.ResponseResult;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/board/like")
 @RequiredArgsConstructor
@@ -29,6 +31,10 @@ public class BoardLikeController {
     @PostMapping("/{id}")
     public ResponseEntity<?> addBoardLike(@AuthenticationPrincipal LoginUser loginUser, @PathVariable("id") Long boardId) {
         Long authId = Long.parseLong(loginUser.getUsername());
+
+        log.debug("boardId: {}", boardId);
+        log.debug("authID: {}", authId);
+
         boardLikeService.add(boardId, authId);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
