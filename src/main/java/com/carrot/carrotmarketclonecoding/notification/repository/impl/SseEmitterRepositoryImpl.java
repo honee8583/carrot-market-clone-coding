eb7656f183a46d92f019665cadfe8bf75f5301d6
honee8583 <honee8583@naver.com>
2024-09-27 19:01:59 +0900

    [FEAT]: SseEmitterRepository 생성
    - SseEmitter와 사용자의 실시간 알림 내용을 저장하기위한 캐시로 구성

---
+++ src/main/java/com/carrot/carrotmarketclonecoding/notification/repository/impl/SseEmitterRepositoryImpl.java
@@ -0,0 +1,67 @@
+package com.carrot.carrotmarketclonecoding.notification.repository.impl;
+
+import com.carrot.carrotmarketclonecoding.notification.repository.SseEmitterRepository;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Repository
+public class SseEmitterRepositoryImpl implements SseEmitterRepository {
+
+    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final Map<String, Object> eventCache = new ConcurrentHashMap<>();
+
+    @Override
+    public SseEmitter save(String emitterId, SseEmitter sseEmitter) {
+        emitters.put(emitterId,sseEmitter);
+        return sseEmitter;
+    }
+
+    @Override
+    public void saveEventCache(String emitterId, Object event) {
+        eventCache.put(emitterId,event);
+    }
+
+    @Override
+    public Map<String, SseEmitter> findAllEmitterStartWithByMemberId(String memberId) {
+        return emitters.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(memberId))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    @Override
+    public Map<String, Object> findAllEventCacheStartWithByMemberId(String memberId) {
+        return eventCache.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(memberId))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    @Override
+    public void deleteById(String emitterId) {
+        emitters.remove(emitterId);
+    }
+
+    @Override
+    public void deleteAllEmitterStartWithId(String memberId) {
+        emitters.forEach(
+                (key,emitter) -> {
+                    if(key.startsWith(memberId)){
+                        emitters.remove(key);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void deleteAllEventCacheStartWithId(String memberId) {
+        eventCache.forEach(
+                (key,emitter) -> {
+                    if(key.startsWith(memberId)){
+                        eventCache.remove(key);
+                    }
+                }
+        );
+    }
+}
