d01b97bbb70dd4ba537cd8782987dd7fd9424c12
honee8583 <honee8583@naver.com>
2024-09-29 21:31:19 +0900

    [FEAT]: 알림 읽음요청 API 구현
    - "Last-Event-ID" 상수화.
    - NotificationServiceImpl -> NotificationService

--- src/main/java/com/carrot/carrotmarketclonecoding/notification/controller/NotificationController.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/notification/controller/NotificationController.java
@@ -1,12 +1,13 @@
 package com.carrot.carrotmarketclonecoding.notification.controller;
 
 import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.GET_ALL_NOTIFICATIONS_SUCCESS;
+import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.READ_NOTIFICATION_SUCCESS;
 
 import com.carrot.carrotmarketclonecoding.auth.dto.LoginUser;
 import com.carrot.carrotmarketclonecoding.common.response.ResponseResult;
 import com.carrot.carrotmarketclonecoding.notification.dto.NotificationResponseDto;
+import com.carrot.carrotmarketclonecoding.notification.service.NotificationService;
 import com.carrot.carrotmarketclonecoding.notification.service.SseEmitterService;
-import com.carrot.carrotmarketclonecoding.notification.service.impl.NotificationServiceImpl;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -23,9 +26,10 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequiredArgsConstructor
 public class NotificationController {
     private final SseEmitterService sseEmitterService;
-    private final NotificationServiceImpl notificationService;
+    private final NotificationService notificationService;
 
     private static final String X_ACCEL_BUFFERING = "X-Accel-Buffering";
+    private static final String LAST_EVENT_ID = "Last-Event-ID";
     private static final String X_ACCEL_BUFFERING_VALUE = "no";
 
     @GetMapping("/notification")
@@ -40,12 +44,19 @@ public class NotificationController {
     @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<SseEmitter> connect(@AuthenticationPrincipal LoginUser loginUser,
                                               HttpServletResponse response,
-                                              @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
+                                              @RequestHeader(value = LAST_EVENT_ID, required = false, defaultValue = "") String lastEventId) {
         response.setHeader(X_ACCEL_BUFFERING, X_ACCEL_BUFFERING_VALUE);
         Long id = Long.parseLong(loginUser.getUsername());
         return ResponseEntity.ok(sseEmitterService.subscribe(id, lastEventId));
     }
 
-    // TODO 읽음처리
+    @PatchMapping("/notification/{id}")
+    public ResponseEntity<?> read(@AuthenticationPrincipal LoginUser loginUser, @PathVariable("id") Long id) {
+        Long authId = Long.parseLong(loginUser.getUsername());
+        notificationService.read(authId, id);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseResult.success(HttpStatus.OK, READ_NOTIFICATION_SUCCESS.getMessage(), null));
+    }
 
 }
