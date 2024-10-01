364f6b71cf4824b84572baa6e99bdca54093ae21
honee8583 <honee8583@naver.com>
2024-10-01 16:56:04 +0900

    [FEAT]: 키워드 응답 Dto 클래스 추가
    - Keyword 엔티티 클래스에 Keyword의 Category엔티티가 존재하지 않을 경우 categoryId null 리턴 로직 구현
    - name, categoryId, minPrice, maxPrice, createDate, updateDate 포함

--- src/main/java/com/carrot/carrotmarketclonecoding/keyword/dto/KeywordResponseDto.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/keyword/dto/KeywordResponseDto.java
@@ -1,8 +1,36 @@
 package com.carrot.carrotmarketclonecoding.keyword.dto;
 
+import com.carrot.carrotmarketclonecoding.keyword.domain.Keyword;
+import java.time.LocalDateTime;
+import lombok.*;
+
 public class KeywordResponseDto {
 
+    @Getter
+    @Setter
+    @Builder
+    @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class KeywordDetailResponseDto {
+        private Long id;
+        private Long categoryId;
+        private String name;
+        private Integer minPrice;
+        private Integer maxPrice;
+        private LocalDateTime createDate;
+        private LocalDateTime updateDate;
 
+        public static KeywordDetailResponseDto createDetail(Keyword keyword) {
+            return KeywordDetailResponseDto.builder()
+                    .id(keyword.getId())
+                    .categoryId(keyword.getCategoryId())
+                    .name(keyword.getName())
+                    .minPrice(keyword.getMinPrice())
+                    .maxPrice(keyword.getMaxPrice())
+                    .createDate(keyword.getCreateDate())
+                    .updateDate(keyword.getUpdateDate())
+                    .build();
+        }
     }
 }
