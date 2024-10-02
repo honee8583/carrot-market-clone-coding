f099873a287c4b836a068fca828321bf0ec76000
honee8583 <honee8583@naver.com>
2024-10-02 21:22:07 +0900

    [FEAT]: KeywordRepository 쿼리메서드 추가
    - 게시글의 제목 + 내용에 있는 단어들을 name 필드로 가지고 있는 중복되지 않는 Keyword 조회

--- src/main/java/com/carrot/carrotmarketclonecoding/keyword/repository/KeywordRepository.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/keyword/repository/KeywordRepository.java
@@ -3,10 +3,11 @@ package com.carrot.carrotmarketclonecoding.keyword.repository;
 import com.carrot.carrotmarketclonecoding.keyword.domain.Keyword;
 import com.carrot.carrotmarketclonecoding.member.domain.Member;
 import java.util.List;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
     int countByMember(Member member);
-
     List<Keyword> findAllByMember(Member member);
+    Set<Keyword> findByNameIn(Set<String> words);
 }
