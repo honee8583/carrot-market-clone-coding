5ed9cc3f4b05c34a0473af7a7c31fd0dd5ed4525
leedahoon <ekgns0508@gmail.com>
2024-10-19 22:33:24 +0900

    [FEAT]: Spring REST Docs 공통 세팅 코드 분리
    - 컨트롤러 테스트 후 내용 출력
    - 한글 깨짐 방지
    - 공통 응답 필드 문서화 코드 중복 제거 및 분리

---
+++ src/test/java/com/carrot/carrotmarketclonecoding/util/RestDocsTestUtil.java
@@ -0,0 +1,50 @@
+package com.carrot.carrotmarketclonecoding.util;
+
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Disabled
+@Import(RestDocsConfig.class)
+@ExtendWith(RestDocumentationExtension.class)
+public class RestDocsTestUtil extends ControllerTestUtil {
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @Autowired
+    protected RestDocumentationResultHandler restDocs;
+
+    @BeforeEach
+    void setUp(WebApplicationContext context, RestDocumentationContextProvider provider) {
+        this.mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
+                .alwaysDo(restDocs)
+                .alwaysDo(MockMvcResultHandlers.print())
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
+
+    protected FieldDescriptor[] createResponseResultDescriptor() {
+        return new FieldDescriptor[] {
+                fieldWithPath("status").description("응답 상태"),
+                fieldWithPath("result").description("응답 결과"),
+                fieldWithPath("message").description("응답 메시지"),
+                fieldWithPath("data").description("응답 본문")
+        };
+    }
+}
