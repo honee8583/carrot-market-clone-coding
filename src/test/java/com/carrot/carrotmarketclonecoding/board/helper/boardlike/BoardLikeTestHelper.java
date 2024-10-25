da3d733d157665ba4027048cfaf0ee48554a397f
leedahoon <ekgns0508@gmail.com>
2024-10-25 21:42:22 +0900

    [REFACTOR]: 생성자에 MockMvc, RestDocumentationResultHandler를 받아 사용
    - 생성자로 받은 객체를 의존성에 전달해 사용
    - @Import문 추가 (BoardLikeControllerTest import문 줄임)

--- src/test/java/com/carrot/carrotmarketclonecoding/board/helper/boardlike/BoardLikeTestHelper.java
+++ src/test/java/com/carrot/carrotmarketclonecoding/board/helper/boardlike/BoardLikeTestHelper.java
@@ -3,58 +3,51 @@ package com.carrot.carrotmarketclonecoding.board.helper.boardlike;
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+import com.carrot.carrotmarketclonecoding.util.ControllerTestUtil;
 import com.carrot.carrotmarketclonecoding.util.ResultFields;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestComponent;
+import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
+@Import(value = {
+        BoardLikeRequestHelper.class,
+        BoardLikeRestDocsHelper.class
+})
 @TestComponent
-public class BoardLikeTestHelper {
+public class BoardLikeTestHelper extends ControllerTestUtil {
 
-    @Autowired
-    private BoardLikeRequestHelper requestHelper;
+    private final BoardLikeRequestHelper requestHelper;
+    private final BoardLikeRestDocsHelper docsHelper;
 
-    @Autowired
-    private BoardLikeRestDocsHelper docsHelper;
-
-    public void assertLikeBoard(MockMvc mvc, ResultFields resultFields, RestDocumentationResultHandler restDocs)
-            throws Exception {
-        assertResponseResult(requestHelper.requestLikeBoard(mvc), resultFields)
-                .andExpect(jsonPath("$.data", equalTo(null)))
-                .andDo(docsHelper.createAddBoardLikeDocument(restDocs));
+    public BoardLikeTestHelper(MockMvc mvc, RestDocumentationResultHandler restDocs) {
+        this.requestHelper = new BoardLikeRequestHelper(mvc);
+        this.docsHelper = new BoardLikeRestDocsHelper(restDocs);
     }
 
-    public void assertGetUserLikedBoardsSuccess(MockMvc mvc,
-                                                ResultFields resultFields,
-                                                RestDocumentationResultHandler restDocs)
+    public void assertLikeBoard(ResultFields resultFields)
             throws Exception {
-        assertResponseResult(requestHelper.requestGetUserLikedBoards(mvc), resultFields)
+        assertResponseResult(requestHelper.requestLikeBoard(), resultFields)
+                .andExpect(jsonPath("$.data", equalTo(null)))
+                .andDo(docsHelper.createAddBoardLikeDocument());
+    }
+
+    public void assertGetUserLikedBoardsSuccess(ResultFields resultFields)
+            throws Exception {
+        assertResponseResult(requestHelper.requestGetUserLikedBoards(), resultFields)
                 .andExpect(jsonPath("$.data.contents.size()", equalTo(2)))
                 .andExpect(jsonPath("$.data.totalPage", equalTo(1)))
                 .andExpect(jsonPath("$.data.totalElements", equalTo(2)))
                 .andExpect(jsonPath("$.data.first", equalTo(true)))
                 .andExpect(jsonPath("$.data.last", equalTo(true)))
                 .andExpect(jsonPath("$.data.numberOfElements", equalTo(2)))
-                .andDo(docsHelper.createGetUserLikedBoardsSuccessDocument(restDocs));
+                .andDo(docsHelper.createGetUserLikedBoardsSuccessDocument());
     }
 
-    public void assertGetUserLikedBoardsFailed(MockMvc mvc,
-                                             ResultFields resultFields,
-                                             RestDocumentationResultHandler restDocs)
+    public void assertGetUserLikedBoardsFailed(ResultFields resultFields)
             throws Exception{
-        assertResponseResult(requestHelper.requestGetUserLikedBoards(mvc), resultFields)
+        assertResponseResult(requestHelper.requestGetUserLikedBoards(), resultFields)
                 .andExpect(jsonPath("$.data", equalTo(null)))
-                .andDo(docsHelper.createGetUserLikedBoardsFailedDocument(restDocs));
-    }
-
-    // TODO 분리
-    private ResultActions assertResponseResult(ResultActions resultActions, ResultFields resultFields) throws Exception {
-        return resultActions
-                .andExpect(resultFields.getResultMatcher())
-                .andExpect(jsonPath("$.status", equalTo(resultFields.getStatus())))
-                .andExpect(jsonPath("$.result", equalTo(resultFields.isResult())))
-                .andExpect(jsonPath("$.message", equalTo(resultFields.getMessage())));
+                .andDo(docsHelper.createGetUserLikedBoardsFailedDocument());
     }
 }
