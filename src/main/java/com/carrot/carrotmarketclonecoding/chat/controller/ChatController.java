246635c1d46db1c73aaf7bcd3afdcbfde874f8ad
leedahoon <ekgns0508@gmail.com>
2024-11-15 16:22:46 +0900

    [FEAT]: 채팅 컨트롤러 로직 구현
    - 테스트를 위해 LoginUser로부터 아이디를 가져오는 것이 아닌 클라이언트가 직접 아이디를 입력하는 것으로 구현

---
+++ src/main/java/com/carrot/carrotmarketclonecoding/chat/controller/ChatController.java
@@ -0,0 +1,75 @@
+package com.carrot.carrotmarketclonecoding.chat.controller;
+
+import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.CREATE_CHAT_ROOM_SUCCESS;
+import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.DELETE_CHAT_ROOM_SUCCESS;
+import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.GET_CHAT_MESSAGES_SUCCESS;
+import static com.carrot.carrotmarketclonecoding.common.response.SuccessMessage.GET_CHAT_ROOMS_SUCCESS;
+
+import com.carrot.carrotmarketclonecoding.auth.dto.LoginUser;
+import com.carrot.carrotmarketclonecoding.chat.dto.ChatMessageResponseDto;
+import com.carrot.carrotmarketclonecoding.chat.dto.ChatRoomRequestDto.ChatRoomCreateRequestDto;
+import com.carrot.carrotmarketclonecoding.chat.dto.ChatRoomResponseDto;
+import com.carrot.carrotmarketclonecoding.chat.service.ChatMessageService;
+import com.carrot.carrotmarketclonecoding.chat.service.ChatRoomService;
+import com.carrot.carrotmarketclonecoding.common.response.ResponseResult;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/chat/room")
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final ChatRoomService chatRoomService;
+    private final ChatMessageService chatMessageService;
+
+    @PostMapping
+    public ResponseEntity<?> createChatRoom(@AuthenticationPrincipal LoginUser loginUser,
+                                            @RequestBody ChatRoomCreateRequestDto chatRoomCreateRequestDto) {
+        // TODO use Login User
+//        String roomNum = chatRoomService.create(Long.parseLong(loginUser.getUsername()), chatRoomCreateRequestDto);
+        String roomNum = chatRoomService.create(chatRoomCreateRequestDto.getSenderId(), chatRoomCreateRequestDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ResponseResult.success(HttpStatus.CREATED, CREATE_CHAT_ROOM_SUCCESS.getMessage(), roomNum));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getChatRooms(@AuthenticationPrincipal LoginUser loginUser) {
+        List<ChatRoomResponseDto> chatRooms = chatRoomService.getChatRooms(Long.parseLong(loginUser.getUsername()));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseResult.success(HttpStatus.OK, GET_CHAT_ROOMS_SUCCESS.getMessage(), chatRooms));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getChatMessages(@AuthenticationPrincipal LoginUser loginUser,
+                                             @PathVariable("id") Long chatRoomId) {
+        List<ChatMessageResponseDto> chatMessages = chatMessageService.getChatMessages(
+                Long.parseLong(loginUser.getUsername()), chatRoomId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseResult.success(HttpStatus.OK, GET_CHAT_MESSAGES_SUCCESS.getMessage(), chatMessages));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteChatRoom(@AuthenticationPrincipal LoginUser loginUser,
+                                            @PathVariable("id") Long chatRoomId) {
+        chatRoomService.delete(Long.parseLong(loginUser.getUsername()), chatRoomId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseResult.success(HttpStatus.OK, DELETE_CHAT_ROOM_SUCCESS.getMessage(), null));
+    }
+}
