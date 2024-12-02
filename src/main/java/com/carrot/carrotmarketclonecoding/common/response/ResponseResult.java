26ed606c58d63036c789bd59e6ae42d26ffa1353
leedahoon <ekgns0508@gmail.com>
2024-12-02 19:31:23 +0900

    [REFACTOR]: 기본생성자 추가

--- src/main/java/com/carrot/carrotmarketclonecoding/common/response/ResponseResult.java
+++ src/main/java/com/carrot/carrotmarketclonecoding/common/response/ResponseResult.java
@@ -2,9 +2,11 @@ package com.carrot.carrotmarketclonecoding.common.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ResponseResult {
     private int status;
