ab528afa479fc02c8b736e36c20f43d61d9f9e50
leedahoon <ekgns0508@gmail.com>
2024-12-03 17:57:35 +0900

    [BUG]: JpaAuditing으로 인한 테스트 코드 버그 해결

--- src/test/java/com/carrot/carrotmarketclonecoding/RepositoryTest.java
+++ src/test/java/com/carrot/carrotmarketclonecoding/RepositoryTest.java
@@ -4,14 +4,19 @@ import com.carrot.carrotmarketclonecoding.board.repository.BoardLikeRepository;
 import com.carrot.carrotmarketclonecoding.board.repository.BoardPictureRepository;
 import com.carrot.carrotmarketclonecoding.board.repository.BoardRepository;
 import com.carrot.carrotmarketclonecoding.chat.repository.ChatRoomRepository;
+import com.carrot.carrotmarketclonecoding.config.JpaAuditingConfig;
 import com.carrot.carrotmarketclonecoding.keyword.repository.KeywordRepository;
 import com.carrot.carrotmarketclonecoding.member.repository.MemberRepository;
 import com.carrot.carrotmarketclonecoding.notification.repository.NotificationRepository;
 import com.carrot.carrotmarketclonecoding.word.repository.WordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+@Import(value = {
+        JpaAuditingConfig.class
+})
 @ActiveProfiles("test")
 @DataJpaTest
 public class RepositoryTest {
